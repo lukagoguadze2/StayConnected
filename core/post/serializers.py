da5d86f39d115c4ba2d48c81d8d86cb279d4f937
Luka Goguadze <lukagoguadze2@gmail.com>
2024-12-01 03:52:26 +0400

    added serializers for GET request

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -1,18 +1,46 @@
 from rest_framework import serializers
+from home.serializers import TagSerializer
+from authentication.serializers import UserProfileSerializer
 from .models import Post
 
 
 class CreatePostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['title', 'description', 'tags']
+        fields = ('author', 'title', 'description', 'tags')
+        read_only_fields = ('author', )
 
     def create(self, validated_data):
         tags = validated_data.pop('tags')
-        post = super().create(validated_data)
+        post = Post.objects.create(**validated_data)
         for tag in tags:
             post.tags.add(tag)
+        post.save()
+
         return post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = UserProfileSerializer(many=False)
+    created_at = serializers.SerializerMethodField()
+    seen_by_user = serializers.SerializerMethodField()  # must be annotated
+    tags = TagSerializer(many=True)
+
+    class Meta:
+        model = Post                         # must be annotated
+        fields = ('id', 'author', 'created_at', 'seen_by_user', 'tags')
+        read_only_fields = ('author', 'created_at', 'seen_by_user')
+
+    def get_created_at(self, obj):
+        return int(obj.date_posted.timestamp())
+
+    def get_seen_by_user(self, obj):
+        return obj.seen_by_user  # must be annotated
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['is_owner'] = self.context['request'].user == instance.author
+        return representation
 
 
 class LikePostSerializer(serializers.Serializer):
