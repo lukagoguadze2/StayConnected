68bae2a8d99179c2c854d57bdf1d39b1fd795ef8
Luka Goguadze <lukagoguadze2@gmail.com>
2024-12-02 06:43:42 +0400

    update admin.py

--- core/comment/admin.py
+++ core/comment/admin.py
@@ -1,20 +1,32 @@
 from django.contrib import admin
 from .models import Comment, CommentReaction
 
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('author', 'post','date_answered',)
-    search_fields = ('author', 'post')
+    list_display = ('author', 'post', 'date_answered', 'is_correct')
+    list_editable = ('is_correct',)
+    list_select_related = ('author', 'post')
     ordering = ('date_answered',)
     list_per_page = 20
     list_max_show_all = 100
     
     fieldsets = (
         (None, {'fields': ('author', 'post', 'content')}),
-        ('Important dates', {'fields': ('created_at',)}),
     )
     
 
 @admin.register(CommentReaction)
 class CommentReactionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('author', 'comment', 'reaction_type')
+    list_select_related = ('author', 'comment')
+    ordering = ('created_at',)
+    list_per_page = 20
+    list_max_show_all = 100
+
+    fieldsets = (
+        (None, {'fields': ('author', 'comment', 'reaction_type')}),
+    )
+
+
+
--- core/post/admin.py
+++ core/post/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Post, PostReaction, PostSeen
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'date_posted')
+    search_fields = ('title',)
+    list_select_related = ('author', )
+    ordering = ('date_posted',)
+    list_per_page = 20
+    list_max_show_all = 100
+
+    fieldsets = (
+        (None, {'fields': ('author', 'title', 'description', 'tags')}),
+    )
+
+
+@admin.register(PostReaction)
+class PostReactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'reaction_type')
+    list_select_related = ('user', 'post')
+    ordering = ('created_at',)
+    list_per_page = 20
+    list_max_show_all = 100
+
+    fieldsets = (
+        (None, {'fields': ('user', 'post', 'reaction_type')}),
+    )
+
+
+@admin.register(PostSeen)
+class PostSeenAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'seen_at')
+    list_select_related = ('post', 'user')
+    ordering = ('seen_at',)
+    list_per_page = 20
+    list_max_show_all = 100
+
+    fieldsets = (
+        (None, {'fields': ('post', 'user', 'seen_at')}),
+    )
+
