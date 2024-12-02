7241ad206fefaabd9fb1b71b5fa1698cf276b091
Luka Goguadze <lukagoguadze2@gmail.com>
2024-12-02 06:53:08 +0400

    added likes/dislikes/comments count and if post has_correct_answer in response json

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -23,23 +23,39 @@ class CreatePostSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     author = UserProfileSerializer(many=False)
     created_at = serializers.SerializerMethodField()
-    seen_by_user = serializers.SerializerMethodField()  # must be annotated
+    title = serializers.CharField()
+    description = serializers.CharField()
+    likes = serializers.IntegerField(source='like_count')  # must be annotated
+    dislikes = serializers.IntegerField(source='dislike_count')  # must be annotated
+    comments = serializers.IntegerField(source='comment_count')  # must be annotated
+    seen_by_user = serializers.BooleanField()  # must be annotated
+    has_correct_answer = serializers.BooleanField()
     tags = TagSerializer(many=True)
 
     class Meta:
         model = Post                         # must be annotated
-        fields = ('id', 'author', 'created_at', 'seen_by_user', 'tags')
-        read_only_fields = ('author', 'created_at', 'seen_by_user')
+        fields = ('id', 'author', 'title', 'description',
+                  'created_at',
+                  'seen_by_user',
+                  'likes', 'dislikes', 'comments',
+                  'has_correct_answer',
+                  'tags')
+
+        read_only_fields = fields
 
     def get_created_at(self, obj):
         return int(obj.date_posted.timestamp())
 
-    def get_seen_by_user(self, obj):
-        return obj.seen_by_user  # must be annotated
-
     def to_representation(self, instance):
         representation = super().to_representation(instance)
+        representation['engagement'] = {
+            'likes': representation.pop('likes'),
+            'dislikes': representation.pop('dislikes'),
+            'comments': representation.pop('comments'),
+        }
         representation['is_owner'] = self.context['request'].user == instance.author
+        representation['has_correct_answer'] = representation.pop('has_correct_answer')
+        representation['seen_by_user'] = representation.pop('seen_by_user')
         return representation
 
 
