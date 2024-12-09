4ddddf5f74cf4f6ff63f5147e6b82e1c13cf8dab
CodeVnebula <chkhikvadzem471@gmail.com>
2024-12-09 16:52:43 +0400

    update

--- core/authentication/views.py
+++ core/authentication/views.py
@@ -247,7 +247,8 @@ class ResetPasswordView(UpdateAPIView):
                 {'detail': 'Password reset successfully'},
                 status=status.HTTP_200_OK
             )
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RefreshToken(TokenRefreshView):
